b3a53f52644c55fc38416d8d2f42aef6d690454b
PhilopateerWagdy <philowagdy20@gmail.com>
2021-08-10 18:11:46 +0200

    Bottom Navigation Bar

--- lib/Radio.dart
+++ lib/Radio.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class sebha extends StatelessWidget {
+class radio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
       body: Center(
-        child: Text('Sebha',style: TextStyle(fontWeight: FontWeight.bold , fontSize: 40),),
+        child: Text('Radio',style: TextStyle(fontWeight: FontWeight.bold , fontSize: 40),),
       ),
 
     );
