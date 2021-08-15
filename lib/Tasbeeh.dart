98375579829c1b05f7d06ac1eee9cd56ca7f4bf2
Youssef-Bassem <65240061+Youssef-Bassem@users.noreply.github.com>
2021-08-15 17:47:45 +0200

    Rotation feature

--- lib/Tasbeeh.dart
+++ lib/Tasbeeh.dart
@@ -9,36 +9,20 @@ const myColor = const Color(0xFFb7935f);
 int tasbeehCounter = 0;
 List<String> tasbeehat = ["سبحان الله","الحمد لله","لا اله الا الله","الله اکبر","لاحولا ولاقوة الابالله"];
 int index = 0;
+double angle = 0;
 
 class Tasbeeh extends StatefulWidget {
   @override
   State<Tasbeeh> createState() => _TasbeehState();
 }
 
-class _TasbeehState extends State<Tasbeeh>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+class _TasbeehState extends State<Tasbeeh> {
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 2),
-      vsync: this,
-    );
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child:Container(
-          //margin: EdgeInsets.all(3),
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("assets/ahades1.png"),
@@ -60,13 +44,19 @@ class _TasbeehState extends State<Tasbeeh>
                   children: [
                     Container(
                       margin: EdgeInsets.fromLTRB(50,0,0,0),
-                      alignment: AlignmentDirectional.topCenter, 
+                      alignment: AlignmentDirectional.topCenter,
                       child: Image.asset("assets/head.png"),
                     ),
                     Container(
-                      margin: EdgeInsets.all(5),
-                      padding: EdgeInsets.fromLTRB(70, 65, 0, 0),
-                      child:Image.asset("assets/body.png"),
+                      // ignore: deprecated_member_use
+                      child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(70, 65, 0, 0),
+                        onPressed: (){increaseTasbeehCounter();},
+                        child: Transform.rotate(
+                          angle: angle,
+                          child: Image.asset("assets/body.png"),
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -111,7 +101,7 @@ class _TasbeehState extends State<Tasbeeh>
                   ),
                   onPressed:(){
                     setState(() {
-                      increaseCounter();
+                      increaseTasbeehCounter();
                     });
                   },
                 ),
@@ -122,20 +112,27 @@ class _TasbeehState extends State<Tasbeeh>
       ),
     );
   }
-}
-
-void increaseCounter(){
-  if(tasbeehCounter < 33){
-    tasbeehCounter++;
+  rotate() async {
+      await Future.delayed(Duration(milliseconds: 5), () {
+        setState(() {
+          angle += 0.1;
+        });
+      });
   }
-  else {
-    tasbeehCounter = 0;
-    indexCounter();
+  void increaseTasbeehCounter(){
+    rotate();
+    if(tasbeehCounter < 33){
+      tasbeehCounter++;
+    }
+    else {
+      tasbeehCounter = 0;
+      indexCounter();
+    }
   }
-}
-void indexCounter(){
-  if(index < 5)
-    index++;
-  if(index == 5)
-    index = 0;
+  void indexCounter(){
+    if(index < 5)
+      index++;
+    if(index == 5)
+      index = 0;
+  }
 }
