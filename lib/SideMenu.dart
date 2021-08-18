f5f6849532f1d5ef19f4d9cf7c958fb66896ffd7
Youssef-Bassem <65240061+Youssef-Bassem@users.noreply.github.com>
2021-08-18 19:01:49 +0200

    modified side menu ui

--- lib/SideMenu.dart
+++ lib/SideMenu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:task4_training/ThemeData.dart';
 import 'appprovider.dart';
 
 class SideMenu extends StatefulWidget {
@@ -11,99 +12,130 @@ class SideMenu extends StatefulWidget {
 
 class _SideMenuState extends State<SideMenu> {
   late AppProvider provider;
+
   @override
   Widget build(BuildContext context) {
     provider = Provider.of<AppProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        centerTitle: true,
-      ),
-
+      // appBar: AppBar(
+      //   title: Text('Settings'),
+      //   centerTitle: true,
+      // ),
       body: Container(
-        width: double.infinity,
-        padding: EdgeInsets.symmetric(vertical: 48,horizontal: 24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            InkWell(
-              onTap: (){
-                changeTheme();
-              },
-              child: myContainer("Theme"),
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(provider.isDarkModeEnabled()
+                  ? "assets/bg.png"
+                  : "assets/ahades1.png"),
+              fit: BoxFit.fill,
             ),
-            InkWell(
-              onTap: (){
-                changeLanguage();
-              },
-              child:myContainer("Language"),
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  "Settings",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                InkWell(
+                  onTap: () {
+                    changeTheme();
+                  },
+                  child: myContainer("Theme"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    changeLanguage();
+                  },
+                  child: myContainer("Language"),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
   }
-  Container myContainer(String itemName){
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 12),
-      child:Text(itemName,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold
+
+  Center myContainer(String itemName) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12),
+        child: Text(
+          itemName,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
         ),
       ),
     );
   }
-  void changeTheme(){
-    showModalBottomSheet(context: context, builder: (builderContext){
-      return Container(
-        child:Column(
-          children: [
-            InkWell(
-              onTap: (){
-                if(!provider.isDarkModeEnabled())
-                {
-                  provider.changeTheme();
-                }
-              },
-              child: myContainer("Dark theme"),
+
+  void changeTheme() {
+    showModalBottomSheet(
+        context: context,
+        builder: (builderContext) {
+          return Container(
+            child: Column(
+              children: [
+                InkWell(
+                  onTap: () {
+                    if (!provider.isDarkModeEnabled()) {
+                      provider.changeTheme();
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Center(child: myContainer("Dark theme")),
+                ),
+                InkWell(
+                  onTap: () {
+                    if (provider.isDarkModeEnabled()) {
+                      provider.changeTheme();
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Center(child: myContainer("Light theme")),
+                ),
+              ],
             ),
-            InkWell(
-              onTap: (){
-                if(provider.isDarkModeEnabled())
-                {
-                  provider.changeTheme();
-                }
-              },
-              child:myContainer("Light theme"),
-            ),
-          ],
-        ),
-      );
-    });
+          );
+        });
   }
-  void changeLanguage(){
-    showModalBottomSheet(context: context, builder: (builderContext){
-      return Container(
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            InkWell(
-                onTap: (){
-                  provider.changeLanguage('en');
-                },
-                child: myContainer("English")
+
+  void changeLanguage() {
+    showModalBottomSheet(
+        context: context,
+        builder: (builderContext) {
+          return Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                InkWell(
+                    onTap: () {
+                      provider.changeLanguage('en');
+                      Navigator.pop(context);
+                    },
+                    child: myContainer("English")),
+                InkWell(
+                    onTap: () {
+                      provider.changeLanguage('ar');
+                      Navigator.pop(context);
+                    },
+                    child: myContainer("العربية")),
+              ],
             ),
-            InkWell(
-                onTap: (){
-                  provider.changeLanguage('ar');
-                },
-                child: myContainer("العربية")
-            ),
-          ],
-        ),
-      );
-    });
+          );
+        });
   }
 }
